18f470480be7cf849d05a1a976bd8d0d8b3b81e2
Enrique Antonio Mejías Valero <eamveurope@gmail.com>
2026-01-29 21:47:54 +0100

    Update and rename analisis_logistica_tiempos.sql to analisis_rentabilidad.sql

--- analisis_logistica_tiempos.sql
+++
@@ -1,9 +0,0 @@
--- Análisis de Tiempos de Envío (Lead Time)
-SELECT 
-    [Modo de envío],
-    COUNT(*) AS Total_Pedidos,
-    -- Calculamos el promedio de días entre pedido y envío
-    AVG(DATEDIFF(day, [Fecha de pedido], [Fecha de envío])) AS Promedio_Dias_Entrega
-FROM Ventas_Retail
-GROUP BY [Modo de envío]
-ORDER BY Promedio_Dias_Entrega ASC;
